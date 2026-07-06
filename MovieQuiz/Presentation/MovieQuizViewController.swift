c80a539b80c22a8952a748c2766fe5b8067c8cd6
Strezh <pstrej@yandex.ru>
2026-07-06 21:43:27 +0300

    Сделал первый экран

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -4,18 +4,117 @@ final class MovieQuizViewController: UIViewController {
     
     // MARK: - IBOutlets
 
-    @IBOutlet weak var yesButton: UIButton!
-    @IBOutlet weak var yesLabel: UILabel!
+    @IBOutlet private weak var questionTitleLabel: UILabel!
+    @IBOutlet private weak var indexLabel: UILabel!
+    @IBOutlet private weak var questionLabel: UILabel!
+    @IBOutlet private weak var previewImageView: UIImageView!
     
+    // MARK: - IBActions
     
+    @IBAction private func didTapYesButton(_ sender: Any) {
+    }
+    @IBAction private func didTapNoButton(_ sender: Any) {
+    }
     
+    // MARK: - Properties
+    
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false)
+    ]
+    
+    private var correctAnswers: Int = 0
+    private var currentQuestionIndex: Int = 0
+    private var uncorrectAnswers: Int = 0
+
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-//        yesLabel.font = UIFont(name: "YSDisplay-Bold", size: 25)
-//        yesLabel.text = "Kyky"
+        setupUI()
+        let currentQuestion = questions[currentQuestionIndex]
+        show(quiz: convert(model: currentQuestion))
     }
+    
+   
+    // MARK: - Private Structures
+    
+    struct QuizQuestion {
+        let image: String
+        let text: String
+        let correctAnswer: Bool
+    }
+    
+    struct QuizStepViewModel {
+      let image: UIImage
+      let question: String
+      let questionNumber: String
+    }
+    
+    
+    // MARK: - Private Methods
+    
+    fileprivate func setupUI() {
+        questionLabel.font = UIFont(name: "YSDisplay-Bold", size: 23)
+        questionTitleLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        questionTitleLabel.text = "Вопрос"
+        indexLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
+    }
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let questionStep = QuizStepViewModel(
+            image: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)"
+        )
+        return questionStep
+    }
+   
+    private func show(quiz step: QuizStepViewModel) {
+        indexLabel.text = "\(step.questionNumber)"
+        previewImageView.image = step.image
+        questionLabel.text = step.question
+    }
+    
+    
 }
 
 /*
