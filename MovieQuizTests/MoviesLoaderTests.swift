c96445b09995b1209a5959ef04fdbb38077905b0
Pavel Strezhnev <pstrej@yandex.ru>
2026-08-15 00:40:31 +0300

    Добавлен фейковый networkClient - StubNetworkClient с моковыми данными

---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,94 @@
+//
+//  MoviesLoaderTests.swift
+//  MovieQuizTests
+//
+//  Created by Pavel Strezh on 15.08.2026.
+//
+
+import XCTest
+@testable import MovieQuiz
+
+class MoviesLoaderTests: XCTestCase {
+    func testSuccessLoading() throws {
+        // Given
+        let loader = MoviesLoader()
+        // When
+        
+        // Так как метод загрузки фильмов асинхронный -- нужно ожидание
+        let expectation = expectation(description: "Loading expectation")
+        
+        loader.loadMovies { result in
+            // Then
+            switch result {
+                case .success(let movies)
+                // сравниваем данные с тем, что мы рассчитывали получить
+                expectation.fulfill()
+                
+            case .failure(_) {
+                XCTFail("Unexpected failure") // Эта функция проваливает тест
+            }
+                
+            }
+        }
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testFailureLoading() throws {
+        // Given
+        
+        // When
+        
+        // Then
+    }
+}
+
+
+struct StubNetworkClient: NetworkRoutingProtocol {
+    
+    enum TestError: Error {
+        case test // тестовая ошибка
+    }
+        
+        let emulateError: Bool // нужен, чтобы заглушка эмулировала либо ошибку сети, либо успешный ответ
+        
+        func fetch(url: URL, handler: @escaping (Result<Data, Error>) -> Void) {
+            if emulateError {
+                handler(.failure(TestError.test))
+            } else {
+                handler(.success(expectedResponse))
+            }
+        }
+    private var expectedResponse: Data {
+        """
+        {
+                   "errorMessage" : "",
+                   "items" : [
+                      {
+                         "crew" : "Dan Trachtenberg (dir.), Amber Midthunder, Dakota Beavers",
+                         "fullTitle" : "Prey (2022)",
+                         "id" : "tt11866324",
+                         "imDbRating" : "7.2",
+                         "imDbRatingCount" : "93332",
+                         "image" : "https://m.media-amazon.com/images/M/MV5BMDBlMDYxMDktOTUxMS00MjcxLWE2YjQtNjNhMjNmN2Y3ZDA1XkEyXkFqcGdeQXVyMTM1MTE1NDMx._V1_Ratio0.6716_AL_.jpg",
+                         "rank" : "1",
+                         "rankUpDown" : "+23",
+                         "title" : "Prey",
+                         "year" : "2022"
+                      },
+                      {
+                         "crew" : "Anthony Russo (dir.), Ryan Gosling, Chris Evans",
+                         "fullTitle" : "The Gray Man (2022)",
+                         "id" : "tt1649418",
+                         "imDbRating" : "6.5",
+                         "imDbRatingCount" : "132890",
+                         "image" : "https://m.media-amazon.com/images/M/MV5BOWY4MmFiY2QtMzE1YS00NTg1LWIwOTQtYTI4ZGUzNWIxNTVmXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_Ratio0.6716_AL_.jpg",
+                         "rank" : "2",
+                         "rankUpDown" : "-1",
+                         "title" : "The Gray Man",
+                         "year" : "2022"
+                      }
+                    ]
+                  }
+        """.data(using: utf8)
+    }
+}
