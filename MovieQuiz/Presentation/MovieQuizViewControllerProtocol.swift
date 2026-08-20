c72b077a28859e9143177efe4288b503e11d02a0
Pavel Strezhnev <pstrej@yandex.ru>
2026-08-20 23:20:49 +0300

    Добавлен код с методами в MovieQuizViewControllerProtocol

--- MovieQuiz/Presentation/MovieQuizViewControllerProtocol.swift
+++ MovieQuiz/Presentation/MovieQuizViewControllerProtocol.swift
@@ -8,5 +8,11 @@
 import Foundation
 
 protocol MovieQuizViewControllerProtocol: AnyObject {
-    
+        func show(quiz step: QuizStepViewModel)
+        func showBorder(answer: Bool)
+        func hideBorder()
+        func setButtonsIsEnabled(to newStatus: Bool)
+        func showLoadingIndicator()
+        func hideLoadingIndicator()
+        func showNetworkError(title: String, message: String)
 }
