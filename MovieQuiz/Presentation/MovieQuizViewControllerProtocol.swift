5099b20ad312c343f12c856296d19c59eecf6d4d
Pavel Strezhnev <pstrej@yandex.ru>
2026-08-20 14:54:30 +0300

    Создан новый файл MovieQuizViewControllerProtocol.swift

---
+++ MovieQuiz/Presentation/MovieQuizViewControllerProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  MovieQuizViewControllerProtocol.swift
+//  MovieQuiz
+//
+//  Created by Pavel Strezh on 20.08.2026.
+//
+
+import Foundation
+
+protocol MovieQuizViewControllerProtocol: AnyObject {
+    
+}
