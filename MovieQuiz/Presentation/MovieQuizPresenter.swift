3a2f887944e51798d208f6772e575e90453243c1
Pavel Strezhnev <pstrej@yandex.ru>
2026-08-25 12:44:42 +0300

    В proceedWithAnswer сокращен код в guard else

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -147,7 +147,7 @@ final class MovieQuizPresenter: QuestionFactoryDelegate, AlertPresenterDelegate 
         viewController?.showBorder(answer: isCorrect)
         viewController?.setButtonsIsEnabled(to: false)
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
-            guard let self = self else { return }
+            guard let self else { return }
             self.proceedToNextQuestionOrResults()
         }
     }
